3383775214a9dcac3bf57ce8ee5b4e4ed4d4c376
pafonbu <pafonbu@gmail.com>
2022-07-26 13:04:17 -0400

    FIX: parametrosFlow en modelo config api

--- api/models/ConfigApiDeudas.js
+++ api/models/ConfigApiDeudas.js
@@ -3,117 +3,116 @@ const Schema = mongoose.Schema;
 
 const ConfigApiDeudas = mongoose.model(
   "config_api_deuda",
-  new Schema(
-    {
-      mensajes: {
-        forbiddenAccess: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        serverError: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        badRequest: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        deudaNoEncontrada: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        pagoPendiente: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        pacienteNoEncontrado: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        datosContactoNoConfirmados: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        emailNoEncontrado: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        emailNoValido: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        flowUnavailable: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        pagoRealizado: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        pagoAnulado: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        pagoRechazado: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        flowError: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        errorPaciente: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        errorPaciente: {
-          titulo: String,
-          mensaje: String,
-          color: String,
-          icono: String,
-        },
-        fakeEmail: {
-          subject: String,
-          currency: String,
-          paymentMethod: Number,
-          timeout: Number,
-          payment_currency: String,
-        },
+  new Schema({
+    mensajes: {
+      forbiddenAccess: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
       },
-      version: Number,
+      serverError: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      badRequest: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      deudaNoEncontrada: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      pagoPendiente: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      pacienteNoEncontrado: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      datosContactoNoConfirmados: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      emailNoEncontrado: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      emailNoValido: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      flowUnavailable: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      pagoRealizado: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      pagoAnulado: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      pagoRechazado: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      flowError: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      errorPaciente: {
+        titulo: String,
+        mensaje: String,
+        color: String,
+        icono: String,
+      },
+      fakeEmail: {
+        subject: String,
+        currency: String,
+        paymentMethod: Number,
+        timeout: Number,
+        payment_currency: String,
+      },
     },
-    { timestamps: true }
-  )
+    parametrosFlow: {
+      subject: String,
+      currency: String,
+      paymentMethod: Number,
+      timeout: Number,
+      payment_currency: String,
+    },
+    version: Number,
+  },
+  { timestamps: true })
 );
 
 module.exports = ConfigApiDeudas;
